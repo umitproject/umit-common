61187d00029222e6ed601183ee24a723509d5995
Zhongjie Wang <wzj401@gmail.com>
2011-06-04 19:01:15 +0800

    There was no end point in the full intercalated expression. Fixed

--- umit/common/CronParser.py
+++ umit/common/CronParser.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
-##
-## CronParser.py
-## 
-## Copyright (C) 2006 Adriano Monteiro Marques
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
-## 
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, write to the Free Software
-## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-##
+#
+# CronParser.py
+# 
+# Copyright (C) 2006 Adriano Monteiro Marques
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
 
 import re
 
@@ -85,7 +85,7 @@ class CronParser:
                     result += numbers_range
             elif self.re_intercalated.match(exp):
                 i = self.re_intercalated.match(exp).groups()
-                result += range(int(i[0]),int(i[1]),int(i[2]))
+                result += range(int(i[0]),int(i[1])+1,int(i[2]))
             elif self.re_alone.match(exp):
                 result.append(int(exp))
             else:
